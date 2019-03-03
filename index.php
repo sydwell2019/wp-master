591bc9a4ff5d613ff2fc894dc2bc7d0e004fd107
sydwell2019 <47029123+sydwell2019@users.noreply.github.com>
2019-03-03 23:26:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- First PHP command -->
         <title>
-            <!-- First PHP command -->
             <?php bloginfo('name'); ?>
         </title>
     </head>
